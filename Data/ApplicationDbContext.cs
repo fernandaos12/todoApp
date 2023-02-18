5f85d028ee162ae18f0ebfcf4dbd6b9f47f4b289
Fernanda Oliveira <fernandaos12@gmail.com>
2023-02-18 19:22:52 -0300

    first commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using todoApp.Models;
+
+namespace todoApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ToDo> Todos {get;set;}
+    }
+}
---
+++ Models/ToDoModel.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace todoApp.Models
+{
+    public class ToDo
+    {
+        public int Id{get;set;}
+        
+        [DisplayName("Titulo")]
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Campo Obrigatório")]
+        public string Title{ get; set; }
+        public bool Done { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime LastUpdateDate { get; set; } = DateTime.Now;
+        public string User { get; set; }    
+   
+        
+    }
+}
